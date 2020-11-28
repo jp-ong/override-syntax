be1196a5e268a6b94d38a4c39a833f3d6156c39d
John Paul Ong <jpong5202@gmail.com>
2020-11-28 12:38:07 +0800

    Update: Now removes invalid orders from user

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -5,13 +5,31 @@ const axios = require("axios");
 const auth = require("../../middleware/auth");
 const url = "https://syntax-api-server.herokuapp.com";
 const date = new Date().toISOString();
+const User = require("../../models/User");
 
 router.get("/", auth, (req, res) => {
   try {
     axios
       .get(`${url}/api/orders/order?id=${req.query.id}`)
       .then((response) => res.status(response.status).json(response.data))
-      .catch(({ response }) => res.status(response.status).json(response.data));
+      .catch(({ response }) => {
+        if (response.status === 404) {
+          User.findById(req.user.id, (error, user) => {
+            if (error) {
+              return res
+                .status(400)
+                .json({ msg: "Something went wrong.", status: 400 });
+            } else {
+              user.orders_list = user.orders_list.filter(
+                (order) => order !== req.query.id
+              );
+              user.save({}, (error, user) => {
+                return res.status(response.status).json(response.data);
+              });
+            }
+          });
+        }
+      });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
